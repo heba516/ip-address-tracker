5ab4adf392108ed5e07fc6952f118547862a4f7c
Heba Yasser <hebay99@gmail.com>
2024-09-06 03:16:17 +0300

    change style

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -7,22 +7,22 @@ const Details = ({ ip, details }: Iprops) => {
   return (
     <div
       className="absolute 
-    top-1/2 sm:top-[60%] md:top-[40%] 
+    top-[40%] sm:top-[46%] md:top-[40%] 
     left-1/2 
     transform -translate-x-1/2 -translate-y-1/2 
     bg-white 
-    w-[85%] md:w-[70%] lg:w-[60%] 
+    w-[85%] md:w-[90%] lg:w-[60%] 
     rounded-lg 
     shadow-lg 
     z-50 
-    flex flex-col lg:flex-row items-start justify-between 
-    p-6 md:py-8 md:px-16 
-    space-y-4 md:space-y-0"
+    flex flex-col md:flex-row items-start justify-between 
+    p-4 md:py-8 md:px-16 
+    space-y-2 md:space-y-0"
     >
       <div
         className="relative 
       text-start 
-      lg:after:content-[''] lg:after:absolute 
+      xl:after:content-[''] xl:after:absolute 
       after:right-0 after:top-1 after:h-[70px] 
       after:w-[2px] after:bg-gray-400 
       pr-4 md:pr-14"
@@ -33,7 +33,7 @@ const Details = ({ ip, details }: Iprops) => {
       <div
         className="relative 
       text-start 
-      lg:after:content-[''] lg:after:absolute 
+      xl:after:content-[''] xl:after:absolute 
       after:right-0 after:top-1 after:h-[70px] 
       after:w-[2px] after:bg-gray-400 
       pr-4 md:pr-14"
@@ -46,7 +46,7 @@ const Details = ({ ip, details }: Iprops) => {
       <div
         className="relative 
       text-start 
-      lg:after:content-[''] lg:after:absolute 
+      xl:after:content-[''] xl:after:absolute 
       after:right-0 after:top-1 after:h-[70px] 
       after:w-[2px] after:bg-gray-400 
       pr-4 md:pr-14"
